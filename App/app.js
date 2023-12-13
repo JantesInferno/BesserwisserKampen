38b806b794a6b27f8d122c366b316081f947f23f
Joel Q <JoelQvist@live.se>
2023-12-13 18:01:38 +0100

    Frågor uppdaterade samt funktioner till det

--- App/app.js
+++ App/app.js
@@ -1,6 +1,7 @@
 window.onload = function() {
-    document.getElementById('start-game').addEventListener('click', function() {
-        document.getElementById('start-container').style.display = 'none';
-        document.getElementById('game-screen').style.display = 'block';
+    document.querySelector('#start-game').addEventListener('click', function() {
+        document.querySelector('#start-container').style.display = 'none';
+        document.querySelector('#game-screen').style.display = 'block';
     });
 };
+
